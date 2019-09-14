11305237e251cada76c5e76d5437fc377c133fc8
mike <mike.golovanov@gmail.com>
2019-09-14 21:36:59 +0300

    javadoc

--- src/main/java/ru/mg/tailrec/TailCall.java
+++ src/main/java/ru/mg/tailrec/TailCall.java
@@ -7,16 +7,39 @@ import java.util.stream.Stream;
 @FunctionalInterface
 public interface TailCall<T> {
 
+    /**
+     * Make next recursive call.
+     * <p>The result is trailing call as {@link TailCall#done} or next deferred recursive call</p>
+     *
+     * @return next recursive call
+     */
     TailCall<T> apply();
 
+    /**
+     * Is this the trailing call of recursion and therefore recursion complete?
+     * Only trailing recursive call should return true.
+     *
+     * @return false
+     */
     default boolean isComplete() {
         return false;
     }
 
+    /**
+     * Result of the recursion computation
+     * @return throw Error for not trailing call else return the result of computation
+     */
     default T result() {
         throw new Error("not implemented");
     }
 
+    /**
+     * Invoke sequence of recursive calls for the making result of computation.
+     * <p>This implementation is based on streams. Alternately you
+     * can use {@link TailCall#invokeWhile()}</p>
+     *
+     * @return recursive algorithm computation result
+     */
     default T invoke() {
         return Stream.iterate(this, TailCall::apply)
                 .filter(TailCall::isComplete)
@@ -25,6 +48,13 @@ public interface TailCall<T> {
                 .result();
     }
 
+    /**
+     * Invoke sequence of recursive calls for the making result of computation.
+     * <p>This implementation is based on while statement. Alternately you
+     * can use {@link TailCall#invoke()}</p>
+     *
+     * @return recursive algorithm computation result
+     */
     default T invokeWhile() {
         TailCall<T> call = this.apply();
         while (!call.isComplete()) {
@@ -33,6 +63,13 @@ public interface TailCall<T> {
         return call.result();
     }
 
+    /**
+     * Make trailing recursive call
+     *
+     * @param value result of trailing recursive call
+     * @param <T> type of result
+     * @return trailing recursive call with result.
+     */
     static <T> TailCall<T> done(final T value) {
         return new TailCall<T>() {
             @Override
